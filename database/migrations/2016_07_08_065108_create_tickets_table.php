7dcaa671a02b30c5f16dfa6cb53966067b4d17ac
julianjay97@gmail.com <julianjay97@gmail.com>
2016-07-08 13:33:00 +0630

    Create Two Migration
    -create two migration for tickets and category table.

---
+++ database/migrations/2016_07_08_065108_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content');
+            $table->integer('priority');
+            $table->boolean('status');
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tickets');
+    }
+}
