afd95aa1002d798040583656ae7259d70c500ded
julianjay97@gmail.com <julianjay97@gmail.com>
2016-07-08 13:49:54 +0630

    Create Model
    -create three models for three tables

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = ['title', 'content', 'priority', 'status', 'user_id', 'category_id'];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public function user()
+    {
+    	$this->belongsTo('App\User');
+    }
+
+    public function category()
+    {
+    	$this->hasOne('App\Category');
+    }
 }
